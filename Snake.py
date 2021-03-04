f5317cfeb2cc645b5e84469fcba528ae2ed3f471
Fábry László <70687838+fabrylaszlo@users.noreply.github.com>
2021-03-04 09:52:02 +0100

    asd

--- Snake.py
+++ Snake.py
@@ -5,6 +5,7 @@ import time
 def Jobbra():
     fej.right(90)
 
+
 def Ballra():
     fej.left(90)
 
@@ -16,14 +17,28 @@ palya.title("Snake")
 palya.tracer(0)
 palya.listen()
 palya.onkey(Jobbra, "Right")
-palya.onkey(Ballra,"Left")
+palya.onkey(Ballra, "Left")
 
 fej = turtle.Turtle()
 fej.shape("triangle")
 fej.penup()
 fej.color("yellow")
 
+kijelzo= turtle.Turtle()
+kijelzo.color("white")
+kijelzo.hideturtle()
+kijelzo.penup()
+kijelzo.goto(0, 0)
+kijelzo.clear()
 while True:
     fej.forward(20)
     palya.update()
-    time.sleep(0.3)
+    if fej.xcor() > 400:
+        kijelzo.write("Game Over", align="center", font=("Arial", 36, "bold"))
+    if fej.xcor() < -400:
+        kijelzo.write("Game Over", align="center", font=("Arial", 36, "bold"))
+    if fej.ycor() > 300:
+        kijelzo.write("Game Over", align="center", font=("Arial", 36, "bold"))
+    if fej.ycor() < -300:
+        kijelzo.write("Game Over", align="center", font=("Arial", 36, "bold"))
+    time.sleep(0.15)
